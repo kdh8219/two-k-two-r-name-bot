7a2ecd61b66b4aa2dbb9ee75c7801711598f1ef6
kdh8219 <65698239+kdh8219@users.noreply.github.com>
2023-04-28 22:39:30 +0900

    get_raw_file

--- src/command/commands.ts
+++ src/command/commands.ts
@@ -7,6 +7,7 @@ import del_user_super from "./commands/del_user_super.js";
 import get_blacklist from "./commands/get_blacklist.js";
 import get_file from "./commands/get_file.js";
 import get_members from "./commands/get_members.js";
+import get_raw_file from "./commands/get_raw_file.js";
 import ping from "./commands/ping.js";
 
 const commands: TCommand[] = [
@@ -17,6 +18,7 @@ const commands: TCommand[] = [
   get_blacklist,
   get_file,
   get_members,
+  get_raw_file,
   ping,
 ];
 export default commands;
---
+++ src/command/commands/get_raw_file.ts
@@ -0,0 +1,61 @@
+import {
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+} from "discord.js";
+
+import firebase from "../../wrapper/firebase.js";
+import { TUser } from "../../functions.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("get_raw_file")
+    .setDescription("get the file")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .setDMPermission(false),
+  async execute(interaction: ChatInputCommandInteraction) {
+    const discord_id = interaction.user.id;
+
+    const members = firebase.collection("members");
+    const blacklist = firebase.collection("blacklist");
+
+    if ((await members.where("discord_id", "==", discord_id).get()).empty) {
+      interaction.editReply({
+        content: "`에러`: 하나 이상의 아이디를 등록해야 합니다.",
+      });
+      return;
+    }
+
+    let blacklist_data: Map<string, string[]> = new Map();
+    (await blacklist.get()).forEach((doc) => {
+      const user = doc.data() as TUser;
+      const data_get = blacklist_data.get(user.discord_id);
+      if (!data_get) {
+        blacklist_data.set(user.discord_id, [user.minecraft_uuid]);
+      } else {
+        data_get.push(user.minecraft_uuid);
+        blacklist_data.set(user.discord_id, data_get);
+      }
+    });
+
+    let member_data: Map<string, string[]> = new Map();
+    (await members.get()).forEach((doc) => {
+      const user = doc.data() as TUser;
+      const data_get = member_data.get(user.discord_id);
+      if (!data_get) {
+        member_data.set(user.discord_id, [user.minecraft_uuid]);
+      } else {
+        data_get.push(user.minecraft_uuid);
+        member_data.set(user.discord_id, data_get);
+      }
+    });
+
+    const buffer_file = {
+      attachment: Buffer.from(
+        JSON.stringify({ blacklist: blacklist_data, members: member_data })
+      ),
+      name: "raw_file.txt",
+    };
+    await interaction.editReply({ files: [buffer_file] });
+  },
+};
