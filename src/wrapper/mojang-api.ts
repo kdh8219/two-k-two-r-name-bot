616f7e52a05a5f223d99dff3b778ec44c3ae9ca5
kdh8219 <65698239+kdh8219@users.noreply.github.com>
2023-05-03 19:24:07 +0900

    fix #5 이제 닉네임이 뺏기더라도 버그가 안납니다!
    이걸 시뮬레이션만 가지고 잡네 역시나야

--- src/wrapper/mojang-api.ts
+++ src/wrapper/mojang-api.ts
@@ -3,24 +3,24 @@ import axios from "axios";
 import TwoWayMap from "../utils/two-way-map.js";
 
 class MojangAPI {
-  private cached: TwoWayMap<string, string> = new TwoWayMap<string, string>(); // uuid, id
+  private cache: TwoWayMap<string, string> = new TwoWayMap<string, string>(); // uuid, id
 
   async getIdFromUUID(minecraft_uuid: string): Promise<string> {
-    let id = this.cached.get_by_first(minecraft_uuid);
+    let id = this.cache.get_by_first(minecraft_uuid);
     if (id) return id;
 
     const request = await axios.get(
       `https://api.mojang.com/user/profile/${minecraft_uuid}`
     );
     id = request.data.name;
-
     if (!id) throw new Error("Failed to get minecraft id from api");
 
-    this.cached.set_by_first(minecraft_uuid, id);
+    this.cache.remove_by_second(id);
+    this.cache.set_by_first(minecraft_uuid, id);
     return id;
   }
   async getUUIDFromId(minecraft_id): Promise<string> {
-    let uuid = this.cached.get_by_second(minecraft_id);
+    let uuid = this.cache.get_by_second(minecraft_id);
     if (uuid) return uuid;
 
     const response = await axios.get(
@@ -29,8 +29,8 @@ class MojangAPI {
     uuid = response.data.id;
     if (!uuid) throw new Error("Failed to get minecraft id from api");
 
-    this.cached.remove_by_first(uuid);
-    this.cached.set_by_second(uuid, minecraft_id);
+    this.cache.remove_by_first(uuid);
+    this.cache.set_by_second(uuid, minecraft_id);
     return uuid;
   }
 }
