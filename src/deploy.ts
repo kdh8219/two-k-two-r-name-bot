1d87da580285e46adffc6204f8cb5b9630cffd0b
kdh8219 <65698239+kdh8219@users.noreply.github.com>
2023-05-05 15:59:00 +0900

    remove unused "as string"

--- src/deploy.ts
+++ src/deploy.ts
@@ -6,9 +6,7 @@ import { getCommands } from "./command/commands.js";
 
 const commands = getCommands();
 // Construct and prepare an instance of the REST module
-const rest = new REST({ version: "10" }).setToken(
-  process.env.DISCORD_TOKEN as string
-);
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 // and deploy your commands!
 (async () => {
@@ -18,8 +16,8 @@ const rest = new REST({ version: "10" }).setToken(
     // The put method is used to fully refresh all commands in the guild with the current set
     await rest.put(
       Routes.applicationGuildCommands(
-        process.env.DISCORD_CLIENT_ID as string,
-        process.env.DISCORD_TARGET_GUILD_ID as string
+        process.env.DISCORD_CLIENT_ID,
+        process.env.DISCORD_TARGET_GUILD_ID
       ),
       {
         body: commands.map((command) => {
