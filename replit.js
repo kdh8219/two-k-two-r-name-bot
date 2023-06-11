5b385a3fe0e973e012a8c6aea820a12b2e435609
kdh8219 <65698239+kdh8219@users.noreply.github.com>
2023-06-11 18:05:13 +0900

    replit.js fix typo error on "repl.it"

--- replit.js
+++ replit.js
@@ -2,7 +2,7 @@ import express from "express";
 const express_app = express();
 
 express_app.get("/", (_, res) => {
-  res.send("<h1>2K2R namebot 24/7 with reple.it</h1>");
+  res.send("<h1>2K2R namebot 24/7 with repl.it</h1>");
 });
 
 express_app.listen(80, () => {
